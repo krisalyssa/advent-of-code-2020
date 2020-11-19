b4db31f6390e6d8957237e32579e49c6130efcb7
Craig S. Cottingham <craig@cottingham.net>
2020-11-18 23:43:11 -0600

    feat: turn main.rs into a guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Input your guess:");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read guess");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low."),
+            Ordering::Greater => println!("Too high."),
+            Ordering::Equal => {
+                println!("Correct!");
+                break;
+            }
+        }
+    }
 }
