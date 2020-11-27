b0057a5e448618068300f4b7212d6c5a67ceafdf
Craig S. Cottingham <craig@cottingham.net>
2020-11-27 00:33:26 -0600

    feat(main.rs): rework to get day from command line

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
+use std::env;
+
 mod day_01;
 
-pub enum Day {
-  _01,
-}
-
 fn main() {
-  let day = Day::_01;
+  let args: Vec<String> = env::args().collect();
+  if args.len() < 2 {
+    eprintln!("usage: {} <day>", args[0]);
+    std::process::exit(1);
+  }
 
-  let result = match day {
-    Day::_01 => day_01::run(),
+  let mut day = args[1].to_string();
+  if day.len() < 2 {
+    day = String::from("0".to_string() + &day);
+  };
+
+  let result = match day.as_str() {
+    "01" => day_01::run(),
+    _ => {
+      eprintln!("unrecognized day '{}'", day);
+      std::process::exit(1);
+    }
   };
 
   if let Some((time_part_1, time_part_2, time_total)) = result {
