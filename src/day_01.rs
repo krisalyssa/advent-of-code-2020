0f6f21bbfa6abd1ecc39ca8b1cc6809ec9d9ca4f
Craig S. Cottingham <craig@cottingham.net>
2020-11-26 23:28:49 -0600

    feat: add day 01 code and data

---
+++ src/day_01.rs
@@ -0,0 +1,88 @@
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::iter::FromIterator;
+use std::time::Instant;
+
+pub fn run() {
+  if let Ok(lines) = load_data("data/day01-input.txt") {
+    let start_part_1 = Instant::now();
+    assert_eq!(3303995, part_1(&lines));
+    let time_part_1 = start_part_1.elapsed().as_micros();
+    let start_part_2 = Instant::now();
+    assert_eq!(4953118, part_2(&lines));
+    let time_part_2 = start_part_2.elapsed().as_micros();
+    let time_total = start_part_1.elapsed().as_micros();
+    println!(
+      "Time: part 1 = {} ms, part 2 = {} ms, total = {} ms",
+      time_part_1, time_part_2, time_total
+    );
+  }
+}
+
+pub fn part_1(lines: &Vec<String>) -> u32 {
+  let answer: u32 = lines
+    .iter()
+    .filter_map(|value| value.parse::<u32>().ok())
+    .map(|value| fuel_for_mass(value))
+    .sum();
+  answer
+}
+
+pub fn part_2(lines: &Vec<String>) -> u32 {
+  let answer: u32 = lines
+    .iter()
+    .filter_map(|value| value.parse::<u32>().ok())
+    .map(|value| total_fuel_for_mass(value))
+    .sum();
+  answer
+}
+
+fn fuel_for_mass(mass: u32) -> u32 {
+  if mass / 3 >= 2 {
+    (mass / 3) - 2
+  } else {
+    0
+  }
+}
+
+fn load_data(filename: &str) -> io::Result<Vec<String>> {
+  let f = File::open(filename)?;
+  let reader = BufReader::new(f);
+  let iter = reader.lines().map(|value| match value {
+    Ok(v) => v,
+    _ => "".to_string(),
+  });
+  let v = Vec::from_iter(iter);
+  Ok(v)
+}
+
+fn total_fuel_for_mass(mass: u32) -> u32 {
+  let additional_fuel = fuel_for_mass(mass);
+  if additional_fuel > 0 {
+    additional_fuel + total_fuel_for_mass(additional_fuel)
+  } else {
+    0
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_fuel_for_mass() {
+    assert_eq!(fuel_for_mass(12), 2);
+    assert_eq!(fuel_for_mass(14), 2);
+    assert_eq!(fuel_for_mass(1969), 654);
+    assert_eq!(fuel_for_mass(100756), 33583);
+  }
+
+  #[test]
+  fn test_total_fuel_for_mass() {
+    assert_eq!(total_fuel_for_mass(14), 2);
+    assert_eq!(total_fuel_for_mass(1969), 966);
+    assert_eq!(total_fuel_for_mass(100756), 50346);
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,12 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+mod day_01;
+
+pub enum Day {
+  _01,
+}
 
 fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    loop {
-        println!("Input your guess:");
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read guess");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too low."),
-            Ordering::Greater => println!("Too high."),
-            Ordering::Equal => {
-                println!("Correct!");
-                break;
-            }
-        }
-    }
+  let day = Day::_01;
+  match day {
+    Day::_01 => day_01::run(),
+  }
 }
