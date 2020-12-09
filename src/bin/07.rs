68d2941e1067a6c6d8ce9f2511c0409f5d3d4966
Craig S. Cottingham <craig@cottingham.net>
2020-12-08 23:49:52 -0600

    feat: add code and tests for day 07 part 2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -16,7 +16,7 @@ pub fn main() {
     day.run(&data);
 
     assert_eq!(287, day.part_1.result);
-    assert_eq!(3435, day.part_2.result);
+    assert_eq!(48160, day.part_2.result);
 
     println!("{}", day.to_string());
   } else {
@@ -26,8 +26,7 @@ pub fn main() {
 }
 
 pub fn part_1(data: &[&str]) -> u64 {
-  let graph = build_graph(&data);
-  // let nodes: Vec<&str> = graph.nodes().collect();
+  let graph = build_graph(&data, false);
 
   if let Ok((paths, _)) = bellman_ford(&graph, "shiny gold") {
     paths.iter().filter(|w| w.is_normal()).count() as u64
@@ -36,40 +35,59 @@ pub fn part_1(data: &[&str]) -> u64 {
   }
 }
 
-pub fn part_2(_data: &[&str]) -> u64 {
-  0
+pub fn part_2(data: &[&str]) -> u64 {
+  let graph = build_graph(&data, true);
+
+  // we have to subtract one here because count_contents() counts the outer bag as well
+  unsafe { (count_contents(&graph, "shiny gold") - 1.0).to_int_unchecked::<u64>() }
 }
 
-fn build_dependencies<'a>(rule: &'a &'a str) -> (&'a str, Vec<&'a str>) {
+fn build_dependencies<'a>(rule: &'a &'a str) -> (&'a str, Vec<(u8, &'a str)>) {
   let parts = split_rule(*rule);
   let outer: &str = parts.get(0).unwrap();
-  let inner: Vec<&str> = split_contents(parts.get(1).unwrap())
+  let inner: Vec<(u8, &str)> = split_contents(parts.get(1).unwrap())
     .iter()
     .map(|bag| extract_bag_descriptor(bag))
+    .filter(std::option::Option::is_some)
+    .map(std::option::Option::unwrap)
     .collect();
   (outer, inner)
 }
 
-fn build_graph<'a>(data: &'a [&'a str]) -> GraphMap<&'a str, f32, Directed> {
+fn build_graph<'a>(data: &'a [&'a str], top_down: bool) -> GraphMap<&'a str, f32, Directed> {
   let mut graph = GraphMap::<&str, f32, Directed>::new();
-  let dependencies: Vec<(&str, Vec<&str>)> = data.iter().map(build_dependencies).collect();
+  let dependencies: Vec<(&str, Vec<(u8, &str)>)> = data.iter().map(build_dependencies).collect();
   for (bag, contents) in dependencies {
     graph.add_node(bag);
-    for c in contents {
-      graph.add_node(c);
-      graph.add_edge(c, bag, 1.0);
+    for (quantity, descriptor) in contents {
+      graph.add_node(descriptor);
+      if top_down {
+        graph.add_edge(bag, descriptor, quantity.into());
+      } else {
+        graph.add_edge(descriptor, bag, quantity.into());
+      }
     }
   }
 
   graph
 }
 
-fn extract_bag_descriptor(bag: &str) -> &str {
-  let regex = Regex::new(r"^\d\s+(.+)\s+bags?.?$").unwrap();
+fn count_contents(graph: &GraphMap<&str, f32, Directed>, outer: &str) -> f32 {
+  graph
+    .neighbors_directed(outer, Outgoing)
+    .map(|inner| (count_contents(graph, inner) * *graph.edge_weight(outer, inner).unwrap_or(&0.0)))
+    .sum::<f32>()
+    + 1.0
+}
+
+fn extract_bag_descriptor(bag: &str) -> Option<(u8, &str)> {
+  let regex = Regex::new(r"^(\d+)\s+(.+)\s+bags?.?$").unwrap();
   if let Some(captures) = regex.captures(bag) {
-    captures.get(1).unwrap().as_str()
+    let quantity: u8 = captures.get(1).unwrap().as_str().parse().ok().unwrap();
+    let descriptor: &str = captures.get(2).unwrap().as_str();
+    Some((quantity, descriptor))
   } else {
-    ""
+    None
   }
 }
 
@@ -107,15 +125,25 @@ mod tests {
 
   #[test]
   fn test_part_2() {
-    let data = vec![];
-    assert_eq!(part_2(&data), 0);
+    let data = vec![
+      "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+      "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+      "bright white bags contain 1 shiny gold bag.",
+      "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+      "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+      "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+      "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+      "faded blue bags contain no other bags.",
+      "dotted black bags contain no other bags.",
+    ];
+    assert_eq!(part_2(&data), 32);
   }
 
   #[test]
   fn test_build_dependencies() {
     assert_eq!(
       build_dependencies(&"light red bags contain 1 bright white bag, 2 muted yellow bags."),
-      ("light red", vec!["bright white", "muted yellow"])
+      ("light red", vec![(1, "bright white"), (2, "muted yellow")])
     );
   }
 
@@ -132,20 +160,63 @@ mod tests {
       "faded blue bags contain no other bags.",
       "dotted black bags contain no other bags.",
     ];
-    let graph = build_graph(&data);
+    let top_down = build_graph(&data, true);
+    assert!(top_down.contains_edge("bright white", "shiny gold"));
+    assert!(top_down.contains_edge("muted yellow", "shiny gold"));
+    assert!(top_down.contains_edge("dark orange", "bright white"));
+    assert!(top_down.contains_edge("dark orange", "muted yellow"));
 
-    assert!(graph.contains_edge("shiny gold", "bright white"));
-    assert!(graph.contains_edge("shiny gold", "muted yellow"));
-    assert!(graph.contains_edge("bright white", "dark orange"));
-    assert!(graph.contains_edge("muted yellow", "dark orange"));
+    let bottom_up = build_graph(&data, false);
+    assert!(bottom_up.contains_edge("shiny gold", "bright white"));
+    assert!(bottom_up.contains_edge("shiny gold", "muted yellow"));
+    assert!(bottom_up.contains_edge("bright white", "dark orange"));
+    assert!(bottom_up.contains_edge("muted yellow", "dark orange"));
+  }
+
+  #[test]
+  fn test_count_contents() {
+    {
+      let data = vec![
+        "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+        "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+        "bright white bags contain 1 shiny gold bag.",
+        "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+        "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+        "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+        "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+        "faded blue bags contain no other bags.",
+        "dotted black bags contain no other bags.",
+      ];
+      let graph = build_graph(&data, true);
+      assert_eq!(count_contents(&graph, "faded blue"), 1.0);
+      assert_eq!(count_contents(&graph, "dotted black"), 1.0);
+      assert_eq!(count_contents(&graph, "vibrant plum"), 12.0);
+      assert_eq!(count_contents(&graph, "shiny gold"), 33.0)
+    }
+    {
+      let data = vec![
+        "shiny gold bags contain 2 dark red bags.",
+        "dark red bags contain 2 dark orange bags.",
+        "dark orange bags contain 2 dark yellow bags.",
+        "dark yellow bags contain 2 dark green bags.",
+        "dark green bags contain 2 dark blue bags.",
+        "dark blue bags contain 2 dark violet bags.",
+        "dark violet bags contain no other bags.",
+      ];
+      let graph = build_graph(&data, true);
+      assert_eq!(count_contents(&graph, "shiny gold"), 127.0)
+    }
   }
 
   #[test]
   fn test_extract_bag_descriptor() {
-    assert_eq!(extract_bag_descriptor("1 bright white bag"), "bright white");
     assert_eq!(
-      extract_bag_descriptor("2 muted yellow bags."),
-      "muted yellow"
+      extract_bag_descriptor("1 bright white bag").unwrap(),
+      (1, "bright white")
+    );
+    assert_eq!(
+      extract_bag_descriptor("2 muted yellow bags.").unwrap(),
+      (2, "muted yellow")
     );
   }
 
