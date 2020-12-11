0699f7c4f75b6198c6b067d8da96f693da06b1f7
Craig S. Cottingham <craig@cottingham.net>
2020-12-10 23:09:15 -0600

    feat: add code and tests for day 10 part 2
    Many thanks to [zedrdave](https://github.com/zedrdave) for a hint on Reddit as to how to approach part 2.

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -14,7 +14,7 @@ pub fn main() {
     day.run(&data);
 
     assert_eq!(1856, day.part_1.result);
-    // assert_eq!(93727241, day.part_2.result);
+    assert_eq!(2314037239808, day.part_2.result);
 
     println!("{}", day.to_string());
   } else {
@@ -25,12 +25,25 @@ pub fn main() {
 
 pub fn part_1(data: &[&str]) -> u64 {
   let adapters = adapters(&data);
-  let (count_1, count_3) = count_differences(&adapters);
+  let count_1 = count_differences_of_span(&adapters, 1);
+  let count_3 = count_differences_of_span(&adapters, 3);
+
   (count_1 * count_3) as u64
 }
 
-pub fn part_2(_data: &[&str]) -> u64 {
-  0
+pub fn part_2(data: &[&str]) -> u64 {
+  let adapters = adapters(&data);
+  let differences = differences(&adapters);
+  let indexes_of_threes = indexes_of_threes(&differences);
+  let span_lengths_of_one = span_lengths_of_one(&indexes_of_threes);
+
+  let max_span = *(span_lengths_of_one.iter().max().unwrap());
+  let tribonacci = tribonacci(max_span as usize);
+
+  span_lengths_of_one
+    .iter()
+    .map(|length| tribonacci[*length as usize] as u64)
+    .product::<u64>()
 }
 
 fn adapters(data: &[&str]) -> Vec<u32> {
@@ -42,25 +55,55 @@ fn adapters(data: &[&str]) -> Vec<u32> {
   parsed_data
 }
 
-fn count_differences(adapters: &Vec<u32>) -> (u32, u32) {
+fn chain_adapters(adapters: &Vec<u32>) -> VecDeque<u32> {
   let mut chain: VecDeque<u32> = VecDeque::from_iter((*adapters).iter().copied());
   chain.push_front(0);
   let largest_adapter: u32 = *chain.back().unwrap();
   let device: u32 = largest_adapter + 3;
   chain.push_back(device);
+  chain
+}
 
-  let mut count_1 = 0;
-  let mut count_3 = 0;
+fn count_differences_of_span(adapters: &Vec<u32>, span: usize) -> usize {
+  differences(adapters)
+    .iter()
+    .filter(|d| **d == span as u32)
+    .count()
+}
 
-  for pair in Vec::from(chain).windows(2) {
-    match pair[1] - pair[0] {
-      1 => count_1 += 1,
-      3 => count_3 += 1,
-      _ => panic!("diff between {} and {} isn't 1 or 3", pair[1], pair[0]),
-    }
+fn differences(adapters: &Vec<u32>) -> Vec<u32> {
+  let chain = chain_adapters(adapters);
+
+  Vec::from(chain)
+    .windows(2)
+    .map(|pair| pair[1] - pair[0])
+    .collect()
+}
+
+fn indexes_of_threes(differences: &Vec<u32>) -> Vec<u32> {
+  differences
+    .iter()
+    .enumerate()
+    .filter(|(_, diff)| **diff == 3)
+    .map(|(ix, _)| (ix + 1) as u32)
+    .collect()
+}
+
+fn span_lengths_of_one(indexes_of_threes: &Vec<u32>) -> Vec<u32> {
+  let mut v: Vec<u32> = Vec::from_iter((*indexes_of_threes).iter().copied());
+  v.insert(0, 0);
+  v.windows(2).map(|pair| pair[1] - pair[0] - 1).collect()
+}
+
+fn tribonacci(to_n: usize) -> Vec<u32> {
+  let mut t: Vec<u32> = vec![1, 1, 2];
+  while t.len() <= to_n {
+    let ix_last = t.len();
+    let ix_first = ix_last - 3;
+    let last_3 = t.get(ix_first..ix_last).unwrap().to_vec();
+    t.push(last_3.iter().sum());
   }
-
-  (count_1, count_3)
+  t
 }
 
 #[cfg(test)]
@@ -69,14 +112,32 @@ mod tests {
 
   #[test]
   fn test_part_1() {
-    let data = vec![];
-    assert_eq!(part_1(&data), 0);
+    {
+      let data = vec!["16", "10", "15", "5", "1", "11", "7", "19", "6", "12", "4"];
+      assert_eq!(part_1(&data), 35);
+    }
+    {
+      let data = vec![
+        "28", "33", "18", "42", "31", "14", "46", "20", "48", "47", "24", "23", "49", "45", "19",
+        "38", "39", "11", "1", "32", "25", "35", "8", "17", "7", "9", "4", "2", "34", "10", "3",
+      ];
+      assert_eq!(part_1(&data), 220);
+    }
   }
 
   #[test]
   fn test_part_2() {
-    let data = vec![];
-    assert_eq!(part_2(&data), 0);
+    {
+      let data = vec!["16", "10", "15", "5", "1", "11", "7", "19", "6", "12", "4"];
+      assert_eq!(part_2(&data), 8);
+    }
+    {
+      let data = vec![
+        "28", "33", "18", "42", "31", "14", "46", "20", "48", "47", "24", "23", "49", "45", "19",
+        "38", "39", "11", "1", "32", "25", "35", "8", "17", "7", "9", "4", "2", "34", "10", "3",
+      ];
+      assert_eq!(part_2(&data), 19208);
+    }
   }
 
   #[test]
@@ -104,11 +165,12 @@ mod tests {
   }
 
   #[test]
-  fn test_count_differences() {
+  fn test_count_differences_of_span() {
     {
       let data = vec!["16", "10", "15", "5", "1", "11", "7", "19", "6", "12", "4"];
       let adapters = adapters(&data);
-      assert_eq!(count_differences(&adapters), (7, 5));
+      assert_eq!(count_differences_of_span(&adapters, 1), 7);
+      assert_eq!(count_differences_of_span(&adapters, 3), 5);
     }
     {
       let data = vec![
@@ -116,7 +178,90 @@ mod tests {
         "38", "39", "11", "1", "32", "25", "35", "8", "17", "7", "9", "4", "2", "34", "10", "3",
       ];
       let adapters = adapters(&data);
-      assert_eq!(count_differences(&adapters), (22, 10));
+      assert_eq!(count_differences_of_span(&adapters, 1), 22);
+      assert_eq!(count_differences_of_span(&adapters, 3), 10);
     }
+  }
+
+  #[test]
+  fn test_differences() {
+    {
+      let data = vec!["16", "10", "15", "5", "1", "11", "7", "19", "6", "12", "4"];
+      let adapters = adapters(&data);
+      assert_eq!(
+        differences(&adapters),
+        vec![1, 3, 1, 1, 1, 3, 1, 1, 3, 1, 3, 3]
+      );
+    }
+    {
+      let data = vec![
+        "28", "33", "18", "42", "31", "14", "46", "20", "48", "47", "24", "23", "49", "45", "19",
+        "38", "39", "11", "1", "32", "25", "35", "8", "17", "7", "9", "4", "2", "34", "10", "3",
+      ];
+      let adapters = adapters(&data);
+      assert_eq!(
+        differences(&adapters),
+        vec![
+          1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 3, 1, 1, 1, 3, 1, 1, 3, 3, 1, 1, 1, 1, 3, 1, 3, 3, 1, 1, 1,
+          1, 3
+        ]
+      );
+    }
+  }
+
+  #[test]
+  fn test_indexes_of_threes() {
+    {
+      let data = vec!["16", "10", "15", "5", "1", "11", "7", "19", "6", "12", "4"];
+      let adapters = adapters(&data);
+      let differences = differences(&adapters);
+      assert_eq!(indexes_of_threes(&differences), vec![2, 6, 9, 11, 12]);
+    }
+    {
+      let data = vec![
+        "28", "33", "18", "42", "31", "14", "46", "20", "48", "47", "24", "23", "49", "45", "19",
+        "38", "39", "11", "1", "32", "25", "35", "8", "17", "7", "9", "4", "2", "34", "10", "3",
+      ];
+      let adapters = adapters(&data);
+      let differences = differences(&adapters);
+      assert_eq!(
+        indexes_of_threes(&differences),
+        vec![5, 10, 11, 15, 18, 19, 24, 26, 27, 32]
+      );
+    }
+  }
+
+  #[test]
+  fn test_span_lengths_of_one() {
+    {
+      let data = vec!["16", "10", "15", "5", "1", "11", "7", "19", "6", "12", "4"];
+      let adapters = adapters(&data);
+      let differences = differences(&adapters);
+      let indexes_of_threes = indexes_of_threes(&differences);
+      assert_eq!(span_lengths_of_one(&indexes_of_threes), vec![1, 3, 2, 1, 0]);
+    }
+    {
+      let data = vec![
+        "28", "33", "18", "42", "31", "14", "46", "20", "48", "47", "24", "23", "49", "45", "19",
+        "38", "39", "11", "1", "32", "25", "35", "8", "17", "7", "9", "4", "2", "34", "10", "3",
+      ];
+      let adapters = adapters(&data);
+      let differences = differences(&adapters);
+      let indexes_of_threes = indexes_of_threes(&differences);
+      assert_eq!(
+        span_lengths_of_one(&indexes_of_threes),
+        vec![4, 4, 0, 3, 2, 0, 4, 1, 0, 4]
+      );
+    }
+  }
+
+  #[test]
+  fn test_tribonacci() {
+    assert_eq!(tribonacci(3), vec![1, 1, 2, 4]);
+    assert_eq!(tribonacci(5), vec![1, 1, 2, 4, 7, 13]);
+    assert_eq!(
+      tribonacci(10),
+      vec![1, 1, 2, 4, 7, 13, 24, 44, 81, 149, 274]
+    );
   }
 }
